1648bd726bbda5e065722d4e636d678b95a87a0a
HarithaRugada <haritharugada@gmail.com>
2020-06-09 11:01:58 +0530

    ADDED : Limit for the daily play

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -5,11 +5,28 @@ echo "WELCOME TO GAMBLING SIMULATION"
 STAKE_PER_DAY=100
 BET_PER_GAME=1
 
-winOrLose=$((RANDOM%2))
+read -p "enter the percent at which the gambler can resign for the day " percent
 
-if [ $winOrLose -eq 1 ]
-then
-	echo "Won the Bet"
-else
-	echo "Lost the Bet"
-fi
+Max_Stake=$(( $STAKE_PER_DAY +($percent*$STAKE_PER_DAY/100) ))
+Min_Stake=$(( $STAKE_PER_DAY -($percent*$STAKE_PER_DAY/100) ))
+
+function dailyPlay()
+{
+	cash=$STAKE_PER_DAY
+
+	while [ $cash -gt $Min_Stake ] && [ $cash -lt $Max_Stake ]
+	do
+		winOrLose=$((RANDOM%2))
+		if [ $winOrLose -eq 1 ]
+		then
+			cash=$(( $cash+$BET_PER_GAME ))
+			echo "Cash is " $cash
+		else
+			cash=$(( $cash-$BET_PER_GAME ))
+			echo "Cash is " $cash
+		fi
+	done
+	echo "Final Cash is " $cash
+}
+
+dailyPlay
